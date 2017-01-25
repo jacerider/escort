df9c0184ebd848e20d863633219058ca55770cc3
Cyle Carlson <jacerider@gmail.com>
2017-01-25 17:02:11 -0600

    Add user access checking to admin path matcher.

--- src/EscortPathMatcher.php
+++ src/EscortPathMatcher.php
@@ -5,6 +5,7 @@ namespace Drupal\escort;
 use Drupal\Core\Path\PathMatcherInterface;
 use Drupal\Core\Path\CurrentPathStack;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Drupal\Core\Session\AccountProxy;
 
 /**
  * Provides a path matcher.
@@ -40,6 +41,13 @@ class EscortPathMatcher implements EscortPathMatcherInterface {
   protected $currentPath;
 
   /**
+   * Drupal\Core\Session\AccountProxy definition.
+   *
+   * @var \Drupal\Core\Session\AccountProxy
+   */
+  protected $currentUser;
+
+  /**
    * Creates a new PathMatcher.
    *
    * @param \Drupal\Core\Path\PathMatcherInterface $path_matcher
@@ -49,10 +57,11 @@ class EscortPathMatcher implements EscortPathMatcherInterface {
    * @param \Drupal\Core\Path\CurrentPathStack $current_path
    *   The current path.
    */
-  public function __construct(PathMatcherInterface $path_matcher, RequestStack $request_stack, CurrentPathStack $current_path) {
+  public function __construct(PathMatcherInterface $path_matcher, RequestStack $request_stack, CurrentPathStack $current_path, AccountProxy $current_user) {
     $this->pathMatcher = $path_matcher;
     $this->requestStack = $request_stack;
     $this->currentPath = $current_path;
+    $this->currentUser = $current_user;
   }
 
   /**
@@ -60,14 +69,19 @@ class EscortPathMatcher implements EscortPathMatcherInterface {
    */
   public function isAdmin() {
     if (!isset($this->isAdmin)) {
-      $pages = ['/admin/config/escort', '/admin/config/escort/*'];
-      $request = $this->requestStack->getCurrentRequest();
-      // Compare the lowercase path alias (if any) and internal path.
-      $path = $this->currentPath->getPath($request);
-      // Do not trim a trailing slash if that is the complete path.
-      $path = $path === '/' ? $path : rtrim($path, '/');
-      // Set static variable.
-      $this->isAdmin = $this->pathMatcher->matchPath($path, implode($pages, "\n"));
+      if (!$this->currentUser->hasPermission('administer escort')) {
+        $this->isAdmin = FALSE;
+      }
+      else {
+        $pages = ['/admin/config/escort', '/admin/config/escort/*'];
+        $request = $this->requestStack->getCurrentRequest();
+        // Compare the lowercase path alias (if any) and internal path.
+        $path = $this->currentPath->getPath($request);
+        // Do not trim a trailing slash if that is the complete path.
+        $path = $path === '/' ? $path : rtrim($path, '/');
+        // Set static variable.
+        $this->isAdmin = $this->pathMatcher->matchPath($path, implode($pages, "\n"));
+      }
     }
     return $this->isAdmin;
   }
