926ec77dca3483c122f2e627c1cb2f87d409815a
Cyle Carlson <jacerider@gmail.com>
2017-02-01 16:39:27 -0600

    Start dropdown plugin.

---
+++ src/Plugin/Escort/Dropdown.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Drupal\escort\Plugin\Escort;
+
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Defines a fallback plugin for missing block plugins.
+ *
+ * @Escort(
+ *   id = "dropdown",
+ *   admin_label = @Translation("Dropdown"),
+ *   category = @Translation("Basic"),
+ * )
+ */
+class Dropdown extends EscortPluginMultipleBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $provideMultiple = TRUE;
+
+  /**
+   * Whether the escort provides multiple sub-escorts.
+   *
+   * @var bool
+   */
+  protected $usesIcon = FALSE;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return array(
+      'trigger' => '',
+      'trigger_icon' => '',
+      'dropdown' => '',
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function escortForm($form, FormStateInterface $form_state) {
+    $form['trigger'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Trigger title'),
+      '#default_value' => $this->configuration['trigger'],
+    ];
+    if ($this->hasIconSupport()) {
+      $form['trigger_icon'] = $this->escortIconForm($form, $form_state);
+      $form['trigger_icon']['#title'] = $this->t('Trigger icon');
+      $form['trigger_icon']['#default_value'] = $this->configuration['trigger_icon'];
+    }
+    $form['dropdown'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Dropdown content'),
+      '#default_value' => $this->configuration['dropdown'],
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function escortSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['trigger'] = $form_state->getValue('trigger');
+    $this->configuration['trigger_icon'] = $form_state->getValue('trigger_icon');
+    $this->configuration['dropdown'] = $form_state->getValue('dropdown');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildItems() {
+    $items = [];
+
+    $items['link'] = $this->buildLink();
+
+    $items['dropdown'] = $this->buildDropdown();
+    $items['dropdown']['#attributes']['class'][] = 'escort-dropdown';
+
+    return $items;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function buildLink() {
+    return [
+      '#markup' => $this->configuration['trigger'],
+      '#icon' => $this->configuration['trigger_icon'],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function buildDropdown() {
+    return [
+      '#markup' => $this->configuration['dropdown'],
+    ];
+  }
+
+}
