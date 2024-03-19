847c11d506dca803969d62cb8c73fc3b0de9a192
Jacob Weber <jacobaweber@gmail.com>
2024-03-19 10:01:13 -0500

    Updates EscortPluginBase for cacheContexts and cacheTags in Drupal 10.2.4

--- src/Plugin/Escort/EscortPluginBase.php
+++ src/Plugin/Escort/EscortPluginBase.php
@@ -18,6 +18,7 @@ use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Cache\Cache;
 use Drupal\Core\Cache\CacheableDependencyInterface;
 use Drupal\Core\Cache\CacheableDependencyTrait;
+use Drupal\Core\Plugin\ContextAwarePluginInterface;
 use Drupal\Core\Plugin\ContextAwarePluginTrait;
 use Drupal\Core\Render\Element;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
@@ -31,7 +32,7 @@ use Drupal\Core\StringTranslation\StringTranslationTrait;
  *
  * @ingroup escort_api
  */
-abstract class EscortPluginBase extends PluginBase implements EscortPluginInterface, PluginWithFormsInterface {
+abstract class EscortPluginBase extends PluginBase implements EscortPluginInterface, PluginWithFormsInterface, ContextAwarePluginInterface {
 
   use ContextAwarePluginTrait {
     getCacheContexts as getContextCacheContexts;
@@ -653,6 +654,50 @@ abstract class EscortPluginBase extends PluginBase implements EscortPluginInterf
     $this->cacheContexts = $cacheability->getCacheContexts();
     $this->cacheTags = $cacheability->getCacheTags();
     $this->cacheMaxAge = $cacheability->getCacheMaxAge();
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addCacheableDependency($other_object) {
+    if ($other_object instanceof CacheableDependencyInterface) {
+      $this->addCacheContexts($other_object->getCacheContexts());
+      $this->addCacheTags($other_object->getCacheTags());
+      $this->mergeCacheMaxAge($other_object->getCacheMaxAge());
+    }
+    else {
+      // Not a cacheable dependency, this can not be cached.
+      $this->cacheMaxAge = 0;
+    }
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addCacheContexts(array $cache_contexts) {
+    if ($cache_contexts) {
+      $this->cacheContexts = Cache::mergeContexts($this->cacheContexts, $cache_contexts);
+    }
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addCacheTags(array $cache_tags) {
+    if ($cache_tags) {
+      $this->cacheTags = Cache::mergeTags($this->cacheTags, $cache_tags);
+    }
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function mergeCacheMaxAge($max_age) {
+    $this->cacheMaxAge = Cache::mergeMaxAges($this->cacheMaxAge, $max_age);
     return $this;
   }
 
